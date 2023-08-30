4fd98072d33f454465bf1954e72be7e3caf581ec
carol <carolbrandes23@gmail.com>
2023-08-30 07:39:06 -0300

    header: application of responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 
 import { Profile } from "./Profile";
 import { NotificationsNav } from "./NotificationsNav";
@@ -6,6 +6,11 @@ import { SearchBox } from "./SearchBox";
 import { Logo } from "./Logo";
 
 export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  }); // * se tiver na versao mobile é false, se tiver na versao desk é verdadeira. Vamos usar para mostrar ou nao o nome do usuario e o email no comp. profile
+
   // *h = "20" - sempre que coloca o numero como String, ele entende q e formato de espacamento
   return (
     <Flex
@@ -19,11 +24,12 @@ export function Header() {
       align="center"
     >
       <Logo />
-      <SearchBox />
+
+      {isWideVersion && <SearchBox />}
 
       <Flex align="center" ml="auto">
         <NotificationsNav />
-        <Profile />
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   );
